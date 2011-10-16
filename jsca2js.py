ebcdc50047b537a81e930d4008b3cf503e99c257
Navin Peiris <navin.peiris@gmail.com>
2011-10-16 18:39:35 +1100

    fixed invalid property names

--- jsca2js.py
+++ jsca2js.py
@@ -5,6 +5,12 @@ from formatter import Formatter
 
 METHOD_INDENTATION = 4
 
+def sanitize(id):
+    if id == 'default':
+        return '_' + id
+    return id
+
+
 def decodeJsonFromFile(filePath):
     try:
         jsonFile = open(filePath, 'r')
@@ -29,6 +35,21 @@ def generateNamespaceJSDoc(namespace):
     return formatter.getResult()
 
 
+def generatePropertyJSDoc(property):
+    formatter = Formatter(METHOD_INDENTATION)
+    formatter.addLine('/**')
+
+    prefix = ' * '
+
+    formatter.addLine(prefix, property['value'])
+    formatter.addLine(prefix, 'platforms:', ', '.join(property['platforms']))
+
+    formatter.addLine(' * ', '@type ', property['type'])
+    formatter.addLine(' * ', '@since ', property['since'])
+    formatter.addLine(' */')
+    return formatter.getResult()
+
+
 def generateMethodJSDoc(method):
     formatter = Formatter(METHOD_INDENTATION)
     formatter.addLine('/**')
@@ -39,7 +60,7 @@ def generateMethodJSDoc(method):
     formatter.addLine(prefix, 'platforms:', ', '.join(method['platforms']))
 
     for param in method['parameters']:
-        formatter.addLine(prefix, '@param {', param['type'], '} ', param['name'], ' ', param['description'])
+        formatter.addLine(prefix, '@param {', param['type'], '} ', sanitize(param['name']), ' ', param['description'])
 
     if method['returntype'] != 'void':
         formatter.addLine(prefix, '@returns {', method['returntype'] + '}')
@@ -50,27 +71,37 @@ def generateMethodJSDoc(method):
 
 
 def formatParams(params):
-    paramNames = [param['name'] for param in params]
+    paramNames = [sanitize(param['name']) for param in params]
     return ', '.join(paramNames)
+
+
+def formatProperties(namespace):
+    formatter = Formatter(METHOD_INDENTATION)
+    for property in namespace['properties']:
+        formatter.add(generatePropertyJSDoc(property))
+        formatter.addLine('this.', sanitize(property['name']), ' = null;')
+        formatter.newLine()
+    return formatter.getResult()
 
 
 def formatMethods(namespace):
     formatter = Formatter(METHOD_INDENTATION)
     for method in namespace['methods']:
         formatter.add(generateMethodJSDoc(method))
-        formatter.addLine('this.', method['name'], ' = function(', formatParams(method['parameters']), ") {")
-        formatter.addLine('}')
+        formatter.addLine('this.', sanitize(method['name']), ' = function(', formatParams(method['parameters']), ") {")
+        formatter.addLine('};')
         formatter.newLine()
     return formatter.getResult()
 
 
 def formatNamespace(namespace):
-    namespaceName = namespace[0]
+    namespaceName = sanitize(namespace[0])
     namespaceContent = namespace[1]
 
     formatter = Formatter()
     formatter.add(generateNamespaceJSDoc(namespaceContent))
     formatter.addLine(namespaceName, ' = (function() {').newLine()
+    formatter.addLine(formatProperties(namespaceContent))
     formatter.addLine(formatMethods(namespaceContent))
     formatter.addLine('}());').newLine()
     return formatter.getResult()
