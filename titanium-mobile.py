b2e6b3d18dcc0673282ca2c1e3b1dccc3067c1c5
Navin Peiris <navin.peiris@gmail.com>
2011-10-19 21:24:45 +1100

    Now automatically retrieves api.json file
    The script now automatically fetches the api.json file for the given version from the web url

--- titanium-mobile.py
+++ titanium-mobile.py
@@ -5,41 +5,65 @@ __copyright__ = "Copyright 2011, Navin Peiris. All rights reserved."
 __email__ = "navinpeiris@gmail.com"
 __status__ = "Development"
 
+import re
+import sys
+import urllib2
 import json
 from jsca2js import convertJsca2Js
 
-TITANIUM_JSCA_FILE_PATH = './api.json'
-TITANIUM_JS_FILE_PATH = './titanium.js'
+DEFAULT_HTTP_TIMEOUT_SECS = 10
 
-def getJsca():
+TITANIUM_VERSION_REGEX = re.compile('\d\.\d\.\d')
+
+def retrieveJsca(version):
+    url = 'http://developer.appcelerator.com/apidoc/mobile/' + version + '/api.json'
     try:
-        jscaFile = open(TITANIUM_JSCA_FILE_PATH, 'r')
-        content = jscaFile.read()
-        jscaFile.close()
-    except IOError:
-        raise Exception('Unable to find JSON API file in path: ' + TITANIUM_JSCA_FILE_PATH)
-
-    return json.JSONDecoder().decode(content)
+        response = urllib2.urlopen(url, timeout=DEFAULT_HTTP_TIMEOUT_SECS)
+        content = response.read()
+        return json.JSONDecoder().decode(content)
+    except urllib2.HTTPError as e:
+        raise Exception('Unable to retrieve API for Titanium version ' + version)
 
 
-def writeJsFile(content):
+def writeJsFile(content, filepath):
     try:
-        file = open(TITANIUM_JS_FILE_PATH, 'w')
+        file = open(filepath, 'w')
         file.write(content)
         file.close()
     except IOError:
         raise Exception('Unable to write JavaScript to file: ' + TITANIUM_JS_FILE_PATH)
 
 
+def errorExit(message=None):
+    if message:
+        sys.stderr.write(message)
+    sys.exit(1)
+
+
 def main():
-    jsca = getJsca()
+    if len(sys.argv) != 2:
+        errorExit('USAGE: ' + sys.argv[0] + ' <titanium-version>')
+
+    version = sys.argv[1]
+
+    if not TITANIUM_VERSION_REGEX.match(version):
+        errorExit('ERROR: Invalid titanium version specified. Must be in the format X.X.X')
+
+    print('Retrieving Titanium JSCA API for version: ' + version)
+    jsca = retrieveJsca(version)
+
+    print('Converting API to JavaScript')
     javascript = convertJsca2Js(jsca)
-    writeJsFile(javascript)
+
+    outputFilePath = 'titanium-mobile-' + version + '.js'
+    print('Writing JavaScript to file: ' + outputFilePath)
+    writeJsFile(javascript, outputFilePath)
 
 if __name__ == '__main__':
     try:
         main()
         print 'Conversion completed successfully'
     except Exception as e:
-        print 'ERROR: ' + e.message
+        print('ERROR: ' + e.message)
+        sys.exit(1)
 
